26298d0e8ac217bf8a49486e351fea97a27e8fa4
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2025-12-03 09:50:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -94,9 +94,9 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 with st.sidebar:
-    st.title("""😈🤪
-    diy diag device by TRHACKNON
-    🤪😈""")
+    st.subheader("""😈🤪""")
+    st.title("""diy diag device by TRHACKNON""")
+    st.title("""🤪😈""")
     st.markdown("""
     <div style="text-align:center;">
         <img src="https://f.top4top.io/p_3624iyyqs0.gif" alt="Logo" style="height:40px; margin-bottom:10px;">
